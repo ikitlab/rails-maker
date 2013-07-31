415a303a982fbeb0d6d04c15b5a44e439bbe1538
Linara Adylova <adylova.linara.r@gmail.com>
2013-07-31 19:05:19 +0500

    change base template

--- templates/base/base.rb
+++ templates/base/base.rb
@@ -1,15 +1,4 @@
-### FILES FOR REMOVE
-run 'rm public/index.html'
-run 'rm public/images/rails.png'
-run 'rm README'
-run 'rm README.rdoc'
-run 'rm public/favicon.ico'
-file 'README.md', <<-FILE
-#{ARGV[0].humanize}
-===========
-FILE
-run 'mkdir app/workers'
-run 'mkdir app/uploaders'
+apply File.expand_path("../lib/remove_files.rb", __FILE__)
 
 ### GIT
 git :init
